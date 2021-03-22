8c9cb3cd322ea0ac353658b05a36c8960f2e1e76
IlyaPrikhach <61990763+IlyaPrikhach@users.noreply.github.com>
2021-03-22 15:13:25 +0300

    Creating base models

---
+++ CarShopWeb/CarShopWeb/Models/Car.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarShopWeb.Models
+{
+    public class Car : Entity
+    {
+        public string Description { get; set; }
+        public DateTime ManufactureDate { get; set; }
+        public int Price { get; set; }
+    }
+}
